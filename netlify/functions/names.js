2487e945fa7aed7c9b9515e12a9d3ad529a413cc
LeeDongJu <22158@daehyun.hs.kr>
2024-07-29 21:31:16 +0900

    .

--- netlify/functions/names.js
+++
@@ -1,8 +0,0 @@
-exports.handler = async () => {
-    const savedNames = ['이름1', '이름2']; // 예시 데이터
-
-    return {
-        statusCode: 200,
-        body: JSON.stringify({ names: savedNames }),
-    };
-};
